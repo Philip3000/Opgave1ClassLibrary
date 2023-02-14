2f3f524486f3da64aef8b0e7c28b7fbed351342e
Philip Preisler <phil313c@edu.zealand.dk>
2023-02-14 11:33:55 +0100

    Add project files.

---
+++ Opgave1ClassLibrary/Models/Car.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Opgave1ClassLibrary.Models
+{
+    public class Car
+    {
+        #region props
+        public int Id { get; set; }
+        public string? Model { get; set; }
+        public int Price { get; set; }
+        public string? LicensePlate { get; set; }
+        #endregion
+        public override string ToString()
+        {
+            return $"{Id}, {Model}, {Price}, {LicensePlate}";
+        }
+
+        #region validations
+        public void ValidateID()
+        {
+            if (Id <= 0)
+            {
+                throw new ArgumentOutOfRangeException();
+            }
+        }
+        public void ValidateModel()
+        {
+            if (Model == null)
+            {
+                throw new ArgumentNullException();
+            }
+            if (Model.Length < 4)
+            {
+                throw new ArgumentOutOfRangeException();
+            }
+        }
+        public void ValidatePrice() 
+        {
+            if (Price < 0) 
+            { 
+                throw new ArgumentOutOfRangeException(); 
+            }
+        }
+        public void ValidateLicensePlate()
+        {
+            if (LicensePlate == null)
+            {
+                throw new ArgumentNullException();
+            }
+            if (LicensePlate.Length <= 2 || LicensePlate.Length >= 7) 
+            { 
+                throw new ArgumentOutOfRangeException(); 
+            }
+        }
+        public void Validate()
+        {
+            ValidateID();
+            ValidateModel();
+            ValidatePrice();
+            ValidateLicensePlate();
+        }
+        #endregion
+    }
+
+}
---
+++ Opgave1ClassLibraryTests/Models/CarTests.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Opgave1ClassLibrary.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace Opgave1ClassLibrary.Models.Tests
+{
+    [TestClass()]
+    public class CarTests
+    {
+        private Car CarIdIsNegative = new() { Id = -10, Model = "FordsNewCar", Price = 10000, LicensePlate = "WG23580" };
+        private Car CarModelIsNull = new() { Id = 1, Model = null, Price = 10000, LicensePlate = "WG23580" };
+        private Car CarLicensePlateIsNull = new() { Id = 6, Model = "Ranger", Price = 10000, LicensePlate = null };
+        private Car CarModelIsTooShort = new() { Id = 5, Model = "911", Price = 10000, LicensePlate = "WG23580" };
+        private Car BadPriceCar = new() { Id = 3, Model = "Volvo", Price = -10, LicensePlate = "WG23580" };
+        private Car BadLicensePlateCar = new() { Id = 4, Model = "Range rover", Price = 10000, LicensePlate = "WG235809202" };
+
+
+        [TestMethod()]
+        public void ValidateIdTest()
+        {
+            Assert.ThrowsException<ArgumentOutOfRangeException>(() => CarIdIsNegative.ValidateID());
+        }
+        [TestMethod()]
+        public void ValidateModelTest()
+        {
+            Assert.ThrowsException<ArgumentOutOfRangeException>(() => CarModelIsTooShort.ValidateModel());
+            Assert.ThrowsException<ArgumentNullException>(() => CarModelIsNull.ValidateModel());
+
+        }
+        [TestMethod()]
+        public void ValidatePriceTest()
+        {
+            Assert.ThrowsException<ArgumentOutOfRangeException>(() => BadPriceCar.ValidatePrice());
+        }
+        [TestMethod()]
+        public void ValidateLicensePlateTest()
+        {
+            Assert.ThrowsException<ArgumentOutOfRangeException>(() => BadLicensePlateCar.ValidateLicensePlate());
+            Assert.ThrowsException<ArgumentNullException>(() => CarLicensePlateIsNull.ValidateLicensePlate());
+
+        }
+
+    }
+}
